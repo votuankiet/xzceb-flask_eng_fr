54a7419e7d8023412735cbaf0fc2b91942a90894
votuankiet <votuankiet162@gmail.com>
2022-09-10 08:22:58 +0000

    Final changes

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -2,21 +2,25 @@ import unittest
 import translator
 
 class TestTranslator(unittest.TestCase):
-    def test_englishToFrench(self):
+    def test_englishToFrench_assertEqual(self):
         # equal
         self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')
-        # not equal
-        self.assertNotEqual(translator.english_to_french('Hello'), 'Hello')
         # null input
         self.assertEqual(translator.english_to_french(None), 'text must be provided')
     
-    def test_frenchToEnglish(self):
+    def test_englishToFrench_assertNotEqual(self):
+        # not equal
+        self.assertNotEqual(translator.english_to_french('Hello'), 'Hello')
+    
+    def test_frenchToEnglish_assertEqual(self):
         # equal
         self.assertEqual(translator.french_to_english('Bonjour'), 'Hello')
-        # not equal
-        self.assertNotEqual(translator.french_to_english('Bonjour'), 'Bonjour')
         # null input
         self.assertEqual(translator.french_to_english(None), 'text must be provided')
 
+    def test_frenchToEnglish_assertNotEqual(self):
+        # not equal
+        self.assertNotEqual(translator.french_to_english('Bonjour'), 'Bonjour')
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
